474bcfe764e820a7caa393688e73d6effe89acf9
Christopher Wilson <clwilson181@gmail.com>
2021-03-17 15:51:40 -0400

    Got slack modal working

--- server.js
+++ server.js
@@ -14,25 +14,46 @@ function insertEvent(name, description, date, time) {
 
 }
 
+let callbackURL
 app.post('/slack', (req, res) => {
-    res.sendStatus(200)
-    console.log(req.body)
+	//Add code to verify password
 
-    /*// Send a slack message later:
-    axios.post(req.body.response_url, {"text": "Your zoom link is: " + zoomLink, "response_type": "ephemeral"})
-    */
-
+    res.status(200).send("")
+	
     axios.post('https://slack.com/api/views.open', {
-        "trigger_id": req.body.trigger_id,
+		"trigger_id": req.body.trigger_id,
         "view": {
-            "type": "modal",
+			"type": "modal",
             "callback_id": "modal-identifier",
             ...popupForm
         }
-    }, {headers: { Authorization: `Bearer ${req.body.token}` }}).then((result) => {
-        console.log(result)
+    }, {headers: {"Authorization": "Bearer xoxb-27149191857-1810892120278-aWAUZ5BRbA9UE1Y08kSLfDxw"}}
+    ).then((result) => {
+		// Send a slack message later:
+		callbackURL = req.body.response_url
     })
+})
 
+app.post('/slackCallback', (req, res) => {
+	const payload = JSON.parse(req.body.payload)
+
+	
+	const A = Object.values(payload.view.state.values).reduce((a, b) => {return {...a, ...b}})
+	const data = {
+		title: A['title'].value,
+		description: A['plain_text_input-action'].value,
+		date: A['datepicker-action'].selected_date,
+		time: A['timepicker-action'].selected_time,
+	}
+	const zoomLink = getZoomLink()
+	axios.post(callbackURL, {"text": `Your zoom link is: ${zoomLink}
+	Data: ${JSON.stringify(data)}`, "response_type": "ephemeral"})
+	
+	setTimeout(() => {
+		axios.post(callbackURL, {"text": `10 seconds later`, "response_type": "ephemeral"})
+	}, 10000)
+
+	res.send( {"response_action": "clear"} )
 })
 
 app.get('/test', (req, res) => {
